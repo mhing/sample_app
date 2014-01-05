a88c9a380aaff4c4dc80901604488469f6b853c5
Matt Hinger <matthew.hinger@gmail.com>
2014-01-04 22:53:43 -0500

    Add Screen to show users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+  # You can have the root of your site routed with "root"
+  root 'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
@@ -8,8 +11,7 @@ SampleApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-   root 'static_pages#home'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
